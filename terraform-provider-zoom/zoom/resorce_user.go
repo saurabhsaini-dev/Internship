90468dd22c35c00fa391a2c75508a162f490ae91
Saurabh Saini <46865678+saurabhsaini-dev@users.noreply.github.com>
2021-04-20 11:43:06 +0530

    Update resorce_user.go

--- terraform-provider-zoom/zoom/resorce_user.go
+++ terraform-provider-zoom/zoom/resorce_user.go
@@ -2,6 +2,7 @@ package zoom
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -11,16 +12,10 @@ func resourceUser() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceUserCreate,
 		ReadContext:   resourceUserRead,
-		UpdateContext: resourceUserUpdate,
-		DeleteContext: resourceUserDelete,
-		// Schema for request 
 		Schema: map[string]*schema.Schema{
-			"action": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"user_info": &schema.Schema{
+			"user": &schema.Schema{
 				Type:     schema.TypeList,
+				MaxItems: 1,
 				Required: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -28,15 +23,11 @@ func resourceUser() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"type": &schema.Schema{
-							Type:     schema.TypeInt,
-							Computed: true,
-						},
-						"first_name": &schema.Schema{
+						"firstname": &schema.Schema{
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"last_name": &schema.Schema{
+						"lastname": &schema.Schema{
 							Type:     schema.TypeString,
 							Computed: true,
 						},
@@ -47,11 +38,19 @@ func resourceUser() *schema.Resource {
 	}
 }
 
-// Implement Create
 func resourceUserCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	c := m.(*hc.Client)
+	// Warning or errors can be collected in a slice type
 
 	var diags diag.Diagnostics
 
+	httpClient := &http.Client{}
+	url := "https://api.zoom.us/v2/users/"
+	httpMethod := http.MethodPost
+	authToken := "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImxOR0pCSGp1Uk9PRktDTTY4TGpIMGciLCJleHAiOjE2MTg3NzA1NjYsImlhdCI6MTYxODc2NTE2OH0.FJcy1sv0ps1iT3eMAd7O9EIcf7adI0QjXSaYMIMkveY"
+
+	user := d.Get("user")
+
 	return diags
 }
+
+
