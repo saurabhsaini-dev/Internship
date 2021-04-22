e396ef5e616f601a73744b8e61a1053241af53af
Saurabh Saini <46865678+saurabhsaini-dev@users.noreply.github.com>
2021-04-22 11:07:09 +0530

    Update resource_user.go

--- terraform-provider-zoom/zoom/resource_user.go
+++ terraform-provider-zoom/zoom/resource_user.go
@@ -4,13 +4,16 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strconv"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
+
+var authToken string
 
 type Userinfo struct {
 	Email     string `json:"email"`
@@ -24,10 +27,24 @@ type CreateUserRequest struct {
 	UserInfo Userinfo `json:"user_info"`
 }
 
+type CreateUserResponse struct {
+	Id        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Type      int    `json:"type"`
+}
+
+type UpdateRequest struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
 func resourceUser() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceUserCreate,
 		ReadContext:   resourceOrderRead,
+		UpdateContext: resourceOrderUpdate,
 		DeleteContext: resourceOrderDelete,
 		Schema: map[string]*schema.Schema{
 			"email": &schema.Schema{
@@ -37,19 +54,19 @@ func resourceUser() *schema.Resource {
 			},
 			"firstname": &schema.Schema{
 				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+				Optional: true,
+				Computed: true,
 			},
 			"lastname": &schema.Schema{
 				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+				Optional: true,
+				Computed: true,
 			},
 		},
 	}
 }
 
-func handleRequest(url string, httpMethod string, body []byte) (response []byte, err error) {
+func handlePostRequest(url string, httpMethod string, body []byte) (response []byte, err error) {
 	httpClient := &http.Client{}
 
 	var req *http.Request
@@ -58,14 +75,18 @@ func handleRequest(url string, httpMethod string, body []byte) (response []byte,
 		return
 	}
 
-	authToken := "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImxOR0pCSGp1Uk9PRktDTTY4TGpIMGciLCJleHAiOjE2MTg5NDM0NDksImlhdCI6MTYxODkzODA1MH0.Bq5i2gbzKKayF3Xj91UqhKtjzQUpngutYCFF_y2jNH8"
-
 	req.Header.Add("Authorization", "Bearer "+authToken)
 	req.Header.Add("Content-Type", "application/json")
 
 	var resp *http.Response
 	resp, err = httpClient.Do(req)
 	if err != nil {
+		return
+	}
+
+	if resp.StatusCode == 409 {
+		er := errors.New("User Already Exists")
+		err = er
 		return
 	}
 
@@ -104,13 +125,23 @@ func resourceUserCreate(ctx context.Context, d *schema.ResourceData, m interface
 		return diag.FromErr(err)
 	}
 
-	_, err = handleRequest(url, httpMethod, reqBody)
+	var b []byte
+	b, err = handlePostRequest(url, httpMethod, reqBody)
 
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	d.SetId(strconv.Itoa(1))
+	var createUserResponse CreateUserResponse
+	err = json.Unmarshal(b, &createUserResponse)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(createUserResponse.Id)
+
+	resourceOrderRead(ctx, d, m)
 
 	return diags
 }
@@ -122,9 +153,88 @@ func resourceOrderRead(ctx context.Context, d *schema.ResourceData, m interface{
 	return diags
 }
 
+func UpdateUser(userID string, ois UpdateRequest) (err error) {
+	var reqBody []byte
+	reqBody, err = json.Marshal(ois)
+	if err != nil {
+		return
+	}
+
+	clienturl := "https://api.zoom.us/v2/users/"
+	url := fmt.Sprintf("%s%s", clienturl, userID)
+	httpMethod := http.MethodPatch
+
+	httpClient := &http.Client{}
+	var req *http.Request
+	req, err = http.NewRequest(httpMethod, url, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return
+	}
+
+	req.Header.Add("Authorization", "Bearer "+authToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	_, err = httpClient.Do(req)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func resourceOrderUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	userID := d.Id()
+
+	if d.HasChange("lastname") {
+		ln := d.Get("lastname").(string)
+		fn := d.Get("firstname").(string)
+		ois := UpdateRequest{
+			FirstName: fn,
+			LastName:  ln,
+		}
+
+		err := UpdateUser(userID, ois)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	return resourceOrderRead(ctx, d, m)
+}
+
+func deleteUser(userID string) (err error) {
+	clienturl := "https://api.zoom.us/v2/users/"
+	url := fmt.Sprintf("%s%s", clienturl, userID)
+	httpMethod := http.MethodDelete
+
+	httpClient := &http.Client{}
+	var req *http.Request
+	req, err = http.NewRequest(httpMethod, url, nil)
+	if err != nil {
+		return
+	}
+
+	req.Header.Add("Authorization", "Bearer "+authToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	_, err = httpClient.Do(req)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func resourceOrderDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	// Warning or errors can be collected in a slice type
 	var diags diag.Diagnostics
+
+	userID := d.Id()
+
+	err := deleteUser(userID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
 	return diags
 }
