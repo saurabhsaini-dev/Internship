a329a11beaa014bc9ba7ffba9fedd2bb397c2adf
Saurabh Saini <46865678+saurabhsaini-dev@users.noreply.github.com>
2021-04-28 11:13:29 +0530

    Create provider_test.go

---
+++ terraform-provider-zoom/zoom/provider_test.go
@@ -0,0 +1,30 @@
+package zoom
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+var testAccProviders map[string]*schema.Provider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider()
+	testAccProviders = map[string]*schema.Provider{
+		"zoom": testAccProvider,
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("ZOOM_TOKEN"); v == "" {
+		t.Fatal("ZOOM_TOKEN must be set for acceptance tests")
+	}
+}
